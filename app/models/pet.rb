860576bc199ba853ee6f44b16aeb4ecc8a10512a
Fernando Sanchez <sanchez5047@gmail.com>
2017-09-20 22:58:55 -0400

    added timezone awareness

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,8 +1,7 @@
 class Pet < ApplicationRecord
     before_create :set_time
     belongs_to :user
-
-    private
+    
     def set_time
         self.lastFed = Time.now
         self.lastPlayed = Time.now
--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module PetApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.active_record.time_zone_aware_types = [:datetime]
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
