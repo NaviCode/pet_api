96807cbb2ac9ee26c6540ac79fcd07e555f58621
Fernando Sanchez <sanchez5047@gmail.com>
2017-09-20 23:21:11 -0400

    date

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,9 +1,9 @@
 class Pet < ApplicationRecord
     before_create :set_time
     belongs_to :user
-    
+
     def set_time
-        self.lastFed = Time.now
-        self.lastPlayed = Time.now
+        self.lastFed = DateTime.now
+        self.lastPlayed = DateTime.now
     end
 end
